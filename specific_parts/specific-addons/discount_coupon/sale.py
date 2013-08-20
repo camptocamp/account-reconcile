2853314aef1c50981bfbf606c021fc77da4b5716
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-08-20 16:11:47 +0200

    [ADD] discount_coupon

---
+++ specific_parts/specific-addons/discount_coupon/sale.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Yannick Vaucher
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+class SaleOrderLine(orm.Model):
+
+    _inherit = 'sale.order.line'
+
+    _columns = {
+        'coupon_id': fields.many2one('sale.discount.coupon', 'Coupon de réduction'),
+        'so_date_order': fields.related('order_id', 'date_order', type='date', string='Date'),
+        'so_partner_id': fields.related('order_id', 'partner_id', type='many2one', relation='res.partner', string='Client'),
+        'so_name': fields.related('order_id', 'name', type='integer', string='No de commande'),
+        }
---
+++ specific_parts/specific-addons/discount_coupon/sale_discount_coupon.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Yannick Vaucher
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import openerp.addons.decimal_precision as dp
+
+from openerp.osv import orm, fields
+
+class SaleDiscountCoupon(orm.Model):
+
+    _name = 'sale.discount.coupon'
+    _inherit = ['mail.thread']
+
+    def _get_residual(self, cr, uid, ids, fields, args, context=None):
+        res = dict.fromkeys(ids, 0)
+
+        for coupon in self.browse(cr, uid, ids, context=context):
+            res[coupon.id] = sum([line.price_subtotal for line in coupon.order_line_ids])
+
+        return res
+
+    _columns = {
+        'name': fields.integer('Numéro de Planning', required=True),
+        'product_id': fields.many2one('product.product', 'Type de bon', required=True, ondelete='cascade'),
+        'date_begin': fields.datetime('Date début de validité', required=True),
+        'date_end': fields.datetime('Date de fin de validité', required=True),
+        'amount': fields.float('Montant du bon', required=True, digits_compute= dp.get_precision('Product Price')),
+        'order_line_ids': fields.one2many('sale.order.line', 'coupon_id', 'Historique', readonly=True),
+        'residual': fields.function(_get_residual, string='Solde', type='integer'),
+        }

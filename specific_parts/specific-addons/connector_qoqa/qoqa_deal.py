f1d7e8e41ff04e274581cfaa7db74f90a14aeeed
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-10-04 17:33:39 +0200

    [ADD] started export of deals

--- specific_parts/specific-addons/connector_qoqa/qoqa_deal.py
+++ specific_parts/specific-addons/connector_qoqa/qoqa_deal.py
@@ -20,6 +20,18 @@
 ##############################################################################
 
 from openerp.osv import orm, fields
+from openerp.addons.connector.unit.mapper import (mapping,
+                                                  changed_by,
+                                                  ExportMapper)
+from openerp.addons.connector.event import (on_record_create,
+                                            on_record_write,
+                                            on_record_unlink,
+                                            )
+from .backend import qoqa
+from . import consumer
+from .unit.export_synchronizer import QoQaExporter
+from .unit.delete_synchronizer import QoQaDeleteSynchronizer
+from .unit.backend_adapter import QoQaAdapter
 
 
 class qoqa_deal(orm.Model):
@@ -33,3 +45,48 @@ class qoqa_deal(orm.Model):
             string='QoQa Backend',
             readonly=True),
     }
+
+
+@on_record_create(model_names='qoqa.deal')
+@on_record_write(model_names='qoqa.deal')
+def delay_export(session, model_name, record_id, fields=None):
+    consumer.delay_export(session, model_name, record_id, fields=fields)
+
+@on_record_unlink(model_names='qoqa.deal')
+def delay_unlink(session, model_name, record_id):
+    consumer.delay_unlink(session, model_name, record_id)
+
+
+@qoqa
+class DealDeleteSynchronizer(QoQaDeleteSynchronizer):
+    """ Deal deleter for QoQa """
+    _model_name = ['qoqa.deal']
+
+
+@qoqa
+class DealExporter(QoQaExporter):
+    _model_name = ['qoqa.deal']
+
+
+@qoqa
+class QoQaDealAdapter(QoQaAdapter):
+    _model_name = 'qoqa.deal'
+    _endpoint = 'deal'
+
+
+@qoqa
+class ProductExportMapper(ExportMapper):
+    _model_name = 'qoqa.deal'
+
+    direct = [('name', 'title'),
+              ('content', 'content'),
+              ('date_begin', 'start_at'),
+              ('date_end', 'end_at'),
+              ]
+
+    @mapping
+    def currency(self, record):
+        currency = record.pricelist_id.currency_id
+        binder = self.get_binder_for_model('res.currency')
+        qoqa_ccy_id = binder.to_backend(currency.id, wrap=True)
+        return {'currency': qoqa_ccy_id}

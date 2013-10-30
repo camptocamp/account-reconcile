44b78622a814dee35f715e865ce6128d80c2a880
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-10-30 16:23:47 +0100

    [ADD] specific_report

---
+++ specific_parts/specific-addons/specific_report/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Yannick Vaucher
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Customer small specific report',
+ 'version': '1.0.0',
+ 'category': 'other',
+ 'description': """
+ Customer specific report
+
+ Contains:
+ - specific report headers
+ """,
+ 'author': 'Camptocamp',
+ 'website': 'http://www.camptocamp.com',
+ 'depends': ['base_headers_webkit',
+             ],
+ 'data': ['base_headers_data.xml',
+          ],
+ 'test': [],
+ 'installable': True,
+ 'active': False,
+ }

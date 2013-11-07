b61b6fcebb1c0299cfaaa7e181315d803631e504
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-11-07 15:22:31 +0100

    [ADD] tests for the iso8601 conversion

--- specific_parts/specific-addons/connector_qoqa/tests/test_iso8601.py
+++ specific_parts/specific-addons/connector_qoqa/tests/test_iso8601.py
@@ -19,28 +19,23 @@
 #
 ##############################################################################
 
-import test_company_binder
-import test_import_metadata
-import test_product_attribute_exporter
-import test_import_partner
-import test_import_offer
-import test_import_product
-import test_import_order
-import test_import_voucher
-import test_iso8601
+import unittest2
+from datetime import datetime
+from ..connector import iso8601_to_utc_datetime
 
-fast_suite = [
-]
 
-checks = [
-    test_company_binder,
-    test_import_metadata,
-    test_product_attribute_exporter,
-    test_import_partner,
-    test_import_offer,
-    test_import_product,
-    test_import_order,
-    test_import_voucher,
-    test_iso8601,
-]
+class test_iso8601(unittest2.TestCase):
 
+    def test_iso8601_to_utc(self):
+        """ convert an iso8601 date to UTC """
+        date = '2013-11-04T13:52:01+0100'
+        utc_date = iso8601_to_utc_datetime(date)
+        expected = datetime(2013, 11, 04, 12, 52, 01)
+        self.assertEquals(utc_date, expected)
+
+    def test_iso8601_notz_to_utc(self):
+        """ convert an iso8601 date without timezone to UTC """
+        date = '2013-11-04T13:52:01'
+        utc_date = iso8601_to_utc_datetime(date)
+        expected = datetime(2013, 11, 04, 13, 52, 01)
+        self.assertEquals(utc_date, expected)

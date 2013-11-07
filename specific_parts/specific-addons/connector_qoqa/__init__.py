216fb925e8175304dd1456fc206ecc18a80c9cf7
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-11-07 11:13:00 +0100

    [FIX] load new packages...

--- specific_parts/specific-addons/connector_qoqa/__init__.py
+++ specific_parts/specific-addons/connector_qoqa/__init__.py
@@ -2,7 +2,8 @@
 
 from . import connector
 from . import backend
-from . import qoqa_model
+from . import qoqa_backend
+from . import qoqa_shop
 from . import unit
 from . import product
 from . import product_template

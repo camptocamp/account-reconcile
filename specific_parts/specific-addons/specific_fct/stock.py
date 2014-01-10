2ac89c7b302b9f6ee3a6a7dc39ff6a45bc24a045
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-01-10 15:22:32 +0100

    [ADD] store field on picking to filter on sum of product on picking

---
+++ specific_parts/specific-addons/specific_fct/stock.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Yannick Vaucher
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import orm, fields
+
+
+class stock_picking(orm.Model):
+    """ Add a number of products field to allow search on it.
+
+    This in order to group picking of a single offer to set other
+    shipping options.
+
+    For exemple: Sending a pair of shoes will be in a small pack and
+    sending two pair of shoes will be in a big pack and we want to
+    change the shipping label options of all those pack. Thus we want
+    a filter that allows us to do that.
+
+    """
+    _inherit = "stock.picking"
+
+    def _get_number_of_products(self, cr, uid, ids, field_names, arg=None, context=None):
+        result = {}
+        for picking in self.browse(cr, uid, ids, context=context):
+            number_of_products = 0
+            for line in picking.move_lines:
+                number_of_products += line.product_qty
+            result[picking.id] = number_of_products
+        return result
+
+    _columns = {
+        'number_of_products': fields.function(
+            _get_number_of_products, method=True,
+            type='integer', string='Number of products',
+            store=True),
+    }
+
+class stock_picking_in(orm.Model):
+    """ Add a number of products field to allow search on it.
+
+    This in order to group picking of a single offer to set other
+    shipping options.
+
+    For exemple: Sending a pair of shoes will be in a small pack and
+    sending two pair of shoes will be in a big pack and we want to
+    change the shipping label options of all those pack. Thus we want
+    a filter that allows us to do that.
+
+    """
+    _inherit = "stock.picking.in"
+
+    def _get_number_of_products(self, cr, uid, ids, field_names, arg=None, context=None):
+        result = {}
+        for picking in self.browse(cr, uid, ids, context=context):
+            number_of_products = 0
+            for line in picking.move_lines:
+                number_of_products += line.product_qty
+            result[picking.id] = number_of_products
+        return result
+
+    _columns = {
+        'number_of_products': fields.function(
+            _get_number_of_products, method=True,
+            type='integer', string='Number of products',
+            store=True),
+    }
+
+class stock_picking_out(orm.Model):
+    """ Add a number of products field to allow search on it.
+
+    This in order to group picking of a single offer to set other
+    shipping options.
+
+    For exemple: Sending a pair of shoes will be in a small pack and
+    sending two pair of shoes will be in a big pack and we want to
+    change the shipping label options of all those pack. Thus we want
+    a filter that allows us to do that.
+
+    """
+    _inherit = "stock.picking.out"
+
+    def _get_number_of_products(self, cr, uid, ids, field_names, arg=None, context=None):
+        result = {}
+        for picking in self.browse(cr, uid, ids, context=context):
+            number_of_products = 0
+            for line in picking.move_lines:
+                number_of_products += line.product_qty
+            result[picking.id] = number_of_products
+        return result
+
+    _columns = {
+        'number_of_products': fields.function(
+            _get_number_of_products, method=True,
+            type='integer', string='Number of products',
+            store=True),
+    }

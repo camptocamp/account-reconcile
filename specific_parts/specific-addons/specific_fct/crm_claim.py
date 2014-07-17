99cbb011817b090da73b1c8d2ef9468a720dcd93
Vincent Renaville <vincent.renaville@camptocamp.com>
2014-07-17 09:42:58 +0200

    [ADD] responsible on claims

--- specific_parts/specific-addons/specific_fct/crm_claim.py
+++ specific_parts/specific-addons/specific_fct/crm_claim.py
@@ -31,9 +31,20 @@ class crm_claim(orm.Model):
     def create(self, cr, uid, vals, context=None):
         claim_id = super(crm_claim, self).create(
             cr, uid, vals, context=context)
+        if vals.has_key('user_id'):
+            if vals['user_id']:
+                # If a suer is selected we remove all previous selected partner
+                fol_obj = self.pool.get('mail.followers')
+                fol_ids = fol_obj.search(cr, uid, [('res_model', '=', self._name), ('res_id', '=', claim_id)])
+                old = [fol.partner_id.id for fol in fol_obj.browse(cr, uid, fol_ids)]
+                self.message_unsubscribe(cr, uid, [claim_id], old, context=context)
+                #
+                user_obj = self.pool.get('res.users')
+                responsible = user_obj.browse(cr,uid,vals['user_id'])
+                self.message_subscribe(cr, uid, [claim_id], [responsible.partner_id.id], context=context)
         if vals.has_key('partner_id'):
             if vals['partner_id']:
-                self.message_subscribe(cr, uid, [claim_id], [vals['partner_id']], context=None)
+                self.message_subscribe(cr, uid, [claim_id], [vals['partner_id']], context=context)
         return claim_id
 
     def write(self, cr, uid, ids, vals, context=None):
@@ -43,10 +54,25 @@ class crm_claim(orm.Model):
                 previous_claim_list = self.browse(cr,uid,ids,context=context)
                 for previous_claim in previous_claim_list:
                     ## If the partner selected differ from the partner previously selected:
-                    if previous_claim.partner_id.id != vals['partner_id']:
-                        self.message_unsubscribe(cr, uid, [previous_claim.id], [previous_claim.partner_id.id], context=context)
+                    if previous_claim.partner_id:
+                        if previous_claim.partner_id.id != vals['partner_id']:
+                            self.message_unsubscribe(cr, uid, [previous_claim.id], [previous_claim.partner_id.id], context=context)
                     ## We subcribe the sected partner to it
                     self.message_subscribe(cr, uid, [previous_claim.id], [vals['partner_id']], context=context)
+        ## Make the same for tu Responsible (user_id):
+        if vals.has_key('user_id'):
+            if vals['user_id']:
+                user_obj = self.pool.get('res.users')
+                responsible = user_obj.browse(cr,uid,vals['user_id'])
+                previous_claim_list = self.browse(cr,uid,ids,context=context)
+                for previous_claim in previous_claim_list:
+                    ## If the partner selected differ from the partner previously selected:
+                    if previous_claim.user_id:
+                        if previous_claim.user_id.id != vals['user_id']:
+                            self.message_unsubscribe(cr, uid, [previous_claim.id], [previous_claim.user_id.partner_id.id], context=context)
+                    ## We subcribe the sected partner to it
+                    self.message_subscribe(cr, uid, [previous_claim.id], [responsible.partner_id.id], context=context)
+
         res = super(crm_claim, self).write(
             cr, uid, ids, vals, context=context)
         return res

912e1ef1542a3a30fe572fd19ff788d7eb3236d7
Tristan Rouiller <tristan.rouiller@gmail.com>
2014-10-06 16:19:54 +0200

    one import per line

--- specific-parts/specific-addons/delivery_carrier_url/__init__.py
+++ specific-parts/specific-addons/delivery_carrier_url/__init__.py
@@ -18,4 +18,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import delivery_carrier, stock_move
+from . import delivery_carrier
+from . import stock_move

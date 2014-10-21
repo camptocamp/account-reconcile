df7d9b2d475ffe4580f723c00315d2483c507a03
Tristan Rouiller <tristan.rouiller@gmail.com>
2014-10-21 15:26:03 +0200

    fix claims width urls overflow and remove unwanted linebreaks

--- specific-parts/specific-addons/crm_claim_mail/crm_claim.py
+++ specific-parts/specific-addons/crm_claim_mail/crm_claim.py
@@ -28,6 +28,7 @@ from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
                            DEFAULT_SERVER_DATETIME_FORMAT,
                            )
 from openerp.addons.email_template import html2text
+from openerp.tools import html2plaintext
 
 
 class crm_claim(orm.Model):
@@ -143,6 +144,10 @@ class crm_claim(orm.Model):
         msg, values = self._complete_from_sale(cr, uid, msg, context=context)
         if values:
             custom_values.update(values)
+
+        desc = html2plaintext(msg.get('body')) if msg.get('body') else ''
+        desc = re.sub(r'(\n){3,}', '\n\n', desc)
+        custom_values['description'] = desc
         return super(crm_claim, self).message_new(
             cr, uid, msg, custom_values=custom_values, context=context)
 

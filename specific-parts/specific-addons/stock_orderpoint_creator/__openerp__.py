12309f3cbe43e7957050a6cff95901b81a2031f4
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-06-19 11:24:15 +0200

    Add a dummy module to be able to uninstall it from DB

---
+++ specific-parts/specific-addons/stock_orderpoint_creator/__openerp__.py
@@ -0,0 +1,15 @@
+{'name': '[DEPRECATED]',
+ 'summary': "stock_orderpoint_creator renamed stock_orderpoint_generator",
+ 'version': '100',
+ 'author': 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'license': 'AGPL-3',
+ 'category': 'category',
+ 'complexity': "easy",  # easy, normal, expert
+ 'depends': ['stock_orderpoint_generator'],
+ 'website': 'www.camptocamp.com',
+ 'data': [],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': False,
+ }

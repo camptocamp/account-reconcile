2050768898f489b50016a007732292bc83c7f05e
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-04-12 14:20:37 +0200

    Set connector_qoqa installable with min. dependencies

--- odoo/local-src/connector_qoqa/__init__.py
+++ odoo/local-src/connector_qoqa/__init__.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
-from . import connector
-from . import backend
-from . import qoqa_backend
-from . import qoqa_shop
-from . import unit
-from . import product
-from . import product_template
-from . import product_attribute
-from . import partner
-from . import address
-from . import sale
-from . import sale_line
-from . import qoqa_offer
-from . import qoqa_offer_position
-from . import qoqa_offer_position_variant
-from . import qoqa_buyphrase
-from . import company
-from . import accounting_issuance
-from . import promo
-from . import picking
-from . import invoice
-from . import static_binding
-from . import wizard
+# from . import connector
+# from . import backend
+# from . import qoqa_backend
+# from . import qoqa_shop
+# from . import unit
+# from . import product
+# from . import product_template
+# from . import product_attribute
+# from . import partner
+# from . import address
+# from . import sale
+# from . import sale_line
+# from . import qoqa_offer
+# from . import qoqa_offer_position
+# from . import qoqa_offer_position_variant
+# from . import qoqa_buyphrase
+# from . import company
+# from . import accounting_issuance
+# from . import promo
+# from . import picking
+# from . import invoice
+# from . import static_binding
+# from . import wizard

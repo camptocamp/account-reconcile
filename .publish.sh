534b5b7dbaaec818ea869188d1367c8cf5b64b5a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-05-23 11:43:55 +0200

    Add more complete docs (#3)
    Add more complete docs
    Conflicts:
    	README.md - Project name specific

--- .publish.sh
+++ .publish.sh
@@ -5,16 +5,16 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Deploying image to docker hub for master (latest)"
-    docker tag qoqa_odoo camptocamp/qoqa_odoo:latest
-    docker push "camptocamp/qoqa_odoo:latest"
+    docker tag qoqa_openerp camptocamp/qoqa_openerp:latest
+    docker push "camptocamp/qoqa_openerp:latest"
   elif [ ! -z "$TRAVIS_TAG" ]; then
     echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
-    docker tag qoqa_odoo camptocamp/qoqa_odoo:${TRAVIS_TAG}
-    docker push "camptocamp/qoqa_odoo:${TRAVIS_TAG}"
+    docker tag qoqa_openerp camptocamp/qoqa_openerp:${TRAVIS_TAG}
+    docker push "camptocamp/qoqa_openerp:${TRAVIS_TAG}"
   elif [ ! -z "$TRAVIS_BRANCH" ]; then
     echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
-    docker tag qoqa_odoo camptocamp/qoqa_odoo:${TRAVIS_BRANCH}
-    docker push "camptocamp/qoqa_odoo:${TRAVIS_BRANCH}"
+    docker tag qoqa_openerp camptocamp/qoqa_openerp:${TRAVIS_BRANCH}
+    docker push "camptocamp/qoqa_openerp:${TRAVIS_BRANCH}"
   else
     echo "Not deploying image"
   fi

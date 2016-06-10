750bdd3327d6d7ae59863f5e0cfbbc3077a41700
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-06-10 16:55:46 +0200

    Add deploy with rancher on qoqa-int

--- travis/publish.sh
+++ travis/publish.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+function deploy {
+    local version=$1
+
+    wget -O - http://releases.rancher.com/compose/beta/v0.7.2/rancher-compose-linux-amd64-v0.7.2.tar.gz |\
+        tar -x -z -C ${HOME} && mv ${HOME}/rancher-compose*/rancher-compose ${HOME}/ || exit $?
+    RANCHER_COMPOSE="${HOME}/rancher-compose"
+    TEMPLATE_DIR="${PWD}/rancher/template/${version}"
+    (. "$HOME/.rancher.env" ; cd "${TEMPLATE_DIR}" && \
+     ${RANCHER_COMPOSE} -p "${RANCHER_PROJECT_NAME}" rm --force && \
+     sleep 30 && \
+     ${RANCHER_COMPOSE} -p "${RANCHER_PROJECT_NAME}" up --pull --recreate --force-recreate --confirm-upgrade -d)
+}
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 
@@ -9,6 +22,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Deploying image to docker hub for master (latest)"
     docker tag qoqa_odoo camptocamp/qoqa_openerp:latest
     docker push "camptocamp/qoqa_openerp:latest"
+    echo "Building integration server"
+    deploy dev
   elif [ ! -z "$TRAVIS_TAG" ]; then
     echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
     docker tag qoqa_odoo camptocamp/qoqa_openerp:${TRAVIS_TAG}

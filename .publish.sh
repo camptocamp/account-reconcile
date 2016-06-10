10cf1661a7a047cb07893c25a86478e2c873d458
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-06-10 09:56:09 +0200

    Fix travis' local image name

--- .publish.sh
+++ .publish.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -e
 
+set -e
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
 
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Deploying image to docker hub for master (latest)"
-    docker tag qoqa_openerp camptocamp/qoqa_openerp:latest
+    docker tag qoqa_odoo camptocamp/qoqa_openerp:latest
     docker push "camptocamp/qoqa_openerp:latest"
   elif [ ! -z "$TRAVIS_TAG" ]; then
     echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
-    docker tag qoqa_openerp camptocamp/qoqa_openerp:${TRAVIS_TAG}
+    docker tag qoqa_odoo camptocamp/qoqa_openerp:${TRAVIS_TAG}
     docker push "camptocamp/qoqa_openerp:${TRAVIS_TAG}"
   elif [ ! -z "$TRAVIS_BRANCH" ]; then
     echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
-    docker tag qoqa_openerp camptocamp/qoqa_openerp:${TRAVIS_BRANCH}
+    docker tag qoqa_odoo camptocamp/qoqa_openerp:${TRAVIS_BRANCH}
     docker push "camptocamp/qoqa_openerp:${TRAVIS_BRANCH}"
   else
     echo "Not deploying image"

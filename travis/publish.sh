c90822f9971474db49912adee53512f5de570d39
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-06-14 11:43:41 +0200

    Ensure filepath is correctly set

--- travis/publish.sh
+++ travis/publish.sh
@@ -9,7 +9,7 @@ function deploy {
         tar -x -z -C ${HOME} && mv ${HOME}/rancher-compose*/rancher-compose ${HOME}/ || exit $?
     RANCHER_COMPOSE="${HOME}/rancher-compose"
     TEMPLATE_DIR="${PWD}/rancher/template/${version}"
-    openssl aes-256-cbc -K $encrypted_deb6d4f0542c_key -iv $encrypted_deb6d4f0542c_iv -in .rancher.env.enc -out .rancher.env -d
+    openssl aes-256-cbc -K $encrypted_deb6d4f0542c_key -iv $encrypted_deb6d4f0542c_iv -in .rancher.env.enc -out "$HOME/.rancher.env" -d
     (. "$HOME/.rancher.env" ; cd "${TEMPLATE_DIR}" && \
      ${RANCHER_COMPOSE} -p "${RANCHER_PROJECT_NAME}" rm --force && \
      sleep 30 && \

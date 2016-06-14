32fb35c69d0a61e7400e28c96b3e2c2f9484cbd2
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-06-14 10:45:52 +0200

    We actually continuously deploy test and not integration

--- travis/publish.sh
+++ travis/publish.sh
@@ -23,7 +23,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Deploying image to docker hub for master (latest)"
     docker tag qoqa_odoo camptocamp/qoqa_openerp:latest
     docker push "camptocamp/qoqa_openerp:latest"
-    echo "Building integration server"
+    echo "Building test server"
     deploy dev
   elif [ ! -z "$TRAVIS_TAG" ]; then
     echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"

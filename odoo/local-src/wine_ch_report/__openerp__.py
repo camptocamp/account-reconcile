e8b0a5849147c0c275157dcc8574f79383da3ea8
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-06-28 16:09:27 +0200

    [PORT] wine_ch_report - CSCV Form

--- odoo/local-src/wine_ch_report/__openerp__.py
+++ odoo/local-src/wine_ch_report/__openerp__.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Authors: Guewen Baconnier, Yannick Vaucher
-#    Copyright 2013 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2013-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {'name': 'Swiss Wine Reports',
  'version': '0.1',
  'category': '',
@@ -35,22 +18,20 @@ the report only on products using a 'Wine' attribute set.
  'maintainer': 'Camptocamp',
  'website': 'http://www.camptocamp.com/',
  'depends': [
-     'base',
-     'report_webkit',
-     'product_custom_attributes',
-     'qoqa_base_data',
+     #'product_custom_attributes',
+     'report',
+     'qoqa_product',
      'stock',
      ],
  'data': [
      'wizard/wine_ch_inventory_wizard_view.xml',
      'report/wine_move_analysis_view.xml',
      'report.xml',
-     'wine_view.xml',
-     'wine_data.xml',
+     'views/report_wine_cscv_form.xml',
      'security/ir.model.access.csv',
      ],
  'test': [],
- 'installable': False,
+ 'installable': True,
  'auto_install': False,
  'application': True,
  'css': [],
--- odoo/local-src/wine_ch_report/wizard/wine_ch_inventory_wizard.py
+++ odoo/local-src/wine_ch_report/wizard/wine_ch_inventory_wizard.py
@@ -1,83 +1,49 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Author: Yannick Vaucher
-#    Copyright Camptocamp SA 2013
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-from openerp.osv import fields, orm
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+from openerp import api, fields, models
 
 
-class WineCHInventoryWizard(orm.TransientModel):
+class WineCHInventoryWizard(models.TransientModel):
     """Will launch wine CH report and pass required args"""
 
     _name = "wine.ch.inventory.wizard"
     _description = "Wine CH Report"
 
-    _columns = {
-        'company_id': fields.many2one('res.company', 'Company',
-                                      required=True),
-        'inventory_date': fields.date('Inventory date'),
-        'location_ids': fields.many2many('stock.location',
-                                         string='Filter on locations'),
-        # TODO replace attribute set by a domain on product_template.is_wine
-        'attribute_set_id': fields.many2one('attribute.set',
-                                            'Wine attribute set'),
-    }
+    @api.model
+    def _default_company(self):
+        company_obj = self.env['res.company']
+        return company_obj._company_default_get('account.invoice')
 
-    def _get_attribute_set_id(self, cr, uid, context=None):
-        """
-        Search for the wine attribute set
-        """
-        attr_obj = self.pool.get('attribute.set')
-        return attr_obj.search(cr, uid,
-                               ['|',
-                                ('name', 'ilike', 'wine'),
-                                ('name', 'ilike', 'vin'),
-                                ],
-                               context=context)
+    company_id = fields.Many2one(
+        'res.company', 'Company',
+        required=True,
+        default=_default_company)
+    inventory_date = fields.Date('Inventory date')
+    location_ids = fields.Many2many('stock.location',
+                                    string='Filter on locations')
+    beverage_type = fields.Selection(
+        [('wine', "Wine"),
+         ('liquor', "Liquor")],
+        "Beverage type",
+        default='wine')
 
-    def _default_company(self, cr, uid, context=None):
-        company_obj = self.pool.get('res.company')
-        return company_obj._company_default_get(cr, uid,
-                                                'account.invoice',
-                                                context=context)
-
-    _defaults = {
-        'attribute_set_id': _get_attribute_set_id,
-        'company_id': _default_company,
-    }
-
-    def print_inventory_report(self, cr, uid, ids, data, context=None):
-        data['form'] = self.read(cr, uid, ids,
-                                 ['inventory_date',
+    @api.multi
+    def print_inventory_report(self, data):
+        data['form'] = self.read(['inventory_date',
                                   'location_ids',
-                                  'attribute_set_id'],
-                                 context=context)[0]
+                                  'beverage_type'],
+                                 )[0]
         return {'type': 'ir.actions.report.xml',
-                'report_name': 'wine.ch.inventory.webkit',
+                'report_name': 'wine_ch_report.report_wine_inventory',
                 'datas': data}
 
-    def print_cscv_form_report(self, cr, uid, ids, data, context=None):
-        data['form'] = self.read(cr, uid, ids,
-                                 ['inventory_date',
+    @api.multi
+    def print_cscv_form_report(self, data):
+        data['form'] = self.read(['inventory_date',
                                   'location_ids',
-                                  'attribute_set_id'],
-                                 context=context)[0]
+                                  'beverage_type'],
+                                 )[0]
         return {'type': 'ir.actions.report.xml',
-                'report_name': 'wine.ch.cscv_form.webkit',
+                'report_name': 'wine_ch_report.report_wine_cscv_form',
                 'datas': data}

743c3fb3e64b0e074c2aebf810fe570f0f59ad3b
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-07-04 13:20:34 +0200

    [PORT] wine_ch_report - README.rst

--- odoo/local-src/wine_ch_report/__openerp__.py
+++ odoo/local-src/wine_ch_report/__openerp__.py
@@ -5,13 +5,6 @@
  'version': '0.1',
  'category': '',
  'description': """
-Swiss Wine Reports
-==================
-
-Add the reports for the "commission fédérale du commerce des vins (CFCV)".
-
-It uses the custom attributes on the products to add the fields used in
-the report only on products using a 'Wine' attribute set.
 
 """,
  'author': 'Camptocamp',

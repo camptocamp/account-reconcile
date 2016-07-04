cb19f6347050a5723ed8bb6210295c6b03c87e22
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-07-04 19:17:59 +0200

    PORT base_stock_picking_pack_split

--- odoo/local-src/base_stock_picking_pack_split/wizard/split_packs.py
+++ odoo/local-src/base_stock_picking_pack_split/wizard/split_packs.py
@@ -1,82 +1,50 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Guewen Baconnier
-#    Copyright 2014 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-from openerp.osv import orm, fields
-from openerp.tools.translate import _
+# Copyright 2014-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+from openerp import _, api, fields, models
 
 
-class picking_split_packs(orm.TransientModel):
-
+class PickingSplitPacks(models.TransientModel):
     _name = 'picking.split.packs'
 
-    _columns = {
-        'qty_per_pack': fields.integer(
-            'Number of product per pack',
-            required=True,
-            help='Defines the maximum number of product in a pack. '
-                 'We consider each product of the packing has the same size.'),
-    }
-
-    _defaults = {
-        'qty_per_pack': 1,
-    }
+    qty_per_pack = fields.Integer(
+        'Number of product per pack',
+        required=True,
+        default=1,
+        help="Defines the maximum number of product in a pack. "
+             "We consider each product of the packing has the same size.")
 
     _sql_constraints = [
         ('qty_per_pack_number', 'CHECK (qty_per_pack > 0)',
          'The number of products per pack must be above 0.'),
     ]
 
-    def _split_packs(self, cr, uid, wizard, picking_ids, context=None):
+    @api.multi
+    def _split_packs(self, pickings):
         """ Fill packs with products
 
         We consider that all product have the same size
 
         """
-        picking_obj = self.pool['stock.picking']
-        picking_obj.prepare_packs(cr, uid, picking_ids,
-                                  max_qty=wizard.qty_per_pack,
-                                  context=context)
-        pickings = picking_obj.browse(cr, uid, picking_ids, context=context)
-        pack_ids = list(set(move.tracking_id.id for picking in pickings
-                            for move in picking.move_lines
-                            if move.tracking_id))
-        return pack_ids
+        pickings.prepare_packs(max_qty=self.qty_per_pack)
+        return pickings.mapped('pack_operation_product_ids.result_package_id')
 
-    def split_packs(self, cr, uid, ids, context=None):
-        if isinstance(ids, (tuple, list)):
-            assert len(ids) == 1, "expect 1 ID, got %s" % ids
-            ids = ids[0]
-        assert context.get('active_ids'), "'active_ids' required"
-        picking_ids = context['active_ids']
+    @api.multi
+    def split_packs(self):
+        self.ensure_one()
+        ctx = self.env.context
+        assert ctx.get('active_ids'), "'active_ids' required"
+        picking_ids = ctx['active_ids']
+        pickings = self.env['stock.picking'].browse(picking_ids)
 
-        wizard = self.browse(cr, uid, ids, context=context)
-        pack_ids = self._split_packs(cr, uid, wizard,
-                                     picking_ids, context=context)
+        packs = self._split_packs(pickings)
 
         return {
-            'domain': "[('id', 'in', %s)]" % pack_ids,
+            'domain': "[('id', 'in', %s)]" % packs.ids,
             'name': _('Generated Packs'),
             'view_type': 'form',
             'view_mode': 'tree,form',
-            'res_model': 'stock.tracking',
+            'res_model': 'stock.quant.package',
             'view_id': False,
             'type': 'ir.actions.act_window',
         }

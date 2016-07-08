b5796fe71b6f570e6ccfcfbfe8fb84f5ad163cfa
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2016-07-08 15:11:54 +0200

    Re-add signature in qoqa_shop

---
+++ odoo/local-src/crm_claim_mail/models/qoqa_shop.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2013-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from openerp import models, fields
+
+
+class QoqaShop(models.Model):
+    _inherit = 'qoqa.shop'
+
+    mail_signature_template = fields.Html(
+        string='Mail signature template',
+        required=True,
+        translate=True,
+        help='This is the mail signature template. You can add some'
+        ' variables :\n'
+        '$user_signature : the current user signature\n'
+        '$user_email : the current user email'
+    )

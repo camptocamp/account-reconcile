0879551bc85469cabc0d3ba3ae072be5014fff01
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-07-25 14:14:37 +0200

    Port purchase report to Qweb

--- odoo/local-src/specific_report/__openerp__.py
+++ odoo/local-src/specific_report/__openerp__.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Yannick Vaucher
-#    Copyright 2013-2014 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {'name': 'Customer small specific report',
- 'version': '1.1.0',
+ 'version': '9.0.1.0.0',
  'category': 'other',
  'description': """
  Customer specific report
@@ -34,19 +16,20 @@
  """,
  'author': 'Camptocamp',
  'website': 'http://www.camptocamp.com',
- 'depends': ['base_headers_webkit',
-             'delivery_carrier_label_default_webkit',
-             'picking_dispatch',
-             'picking_dispatch_group',
-             'purchase_order_webkit',
-             'base_report_assembler'],
- 'data': ['base_headers_data.xml',
-          'reports.xml',
-          'stock_report.xml',
-          'picking_dispatch_assemble.xml',
-          'security/ir.model.access.csv',
+ 'depends': ['purchase',
+             #'delivery_carrier_label_default_webkit',
+             #'picking_dispatch',
+             #'picking_dispatch_group',
+             #'base_report_assembler'
+             ],
+ 'data': [#'base_headers_data.xml',
+          #'reports.xml',
+          #'stock_report.xml',
+          #'picking_dispatch_assemble.xml',
+          #'security/ir.model.access.csv',
+          'views/report_purchaseorder.xml',
           ],
  'test': [],
- 'installable': False,
+ 'installable': True,
  'active': False,
  }

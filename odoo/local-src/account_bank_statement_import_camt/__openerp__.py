7b81cf0a04ca942e38bebe8993ede3d86a203c5c
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2016-09-06 15:26:49 +0200

    Add warning in description

--- odoo/local-src/account_bank_statement_import_camt/__openerp__.py
+++ odoo/local-src/account_bank_statement_import_camt/__openerp__.py
@@ -6,10 +6,13 @@
     'category': 'Accounting & Finance',
     'depends': ['account_bank_statement_import'],
     'description': """
-Module to import CAMT bank statements.
+Module to import CAMT bank statements (BACKPORT FROM ENTERPRISE V10.0!!!).
 ======================================
 
 Improve the import of bank statement feature to support the SEPA recommanded Cash Management format (CAMT.053).
+
+THIS MODULE IS A BACKPORT OF A MODULE IN ODOO ENTERPRISE 10.0. IT SHOULD NOT BE PUBLISHED ELSEWHERE,
+THE LICENSE FORBIDS IT.
     """,
     'data': [
         'data/account_bank_statement_import_data.xml'

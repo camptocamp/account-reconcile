ece507df6da939cf837b84f8e5c30a762547544a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-03-28 17:04:18 +0200

    Add logging in address migration

--- odoo/songs/migration/post_addresses.py
+++ odoo/songs/migration/post_addresses.py
@@ -2,6 +2,8 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
+import time
+
 from pkg_resources import resource_stream
 
 import anthem
@@ -55,7 +57,12 @@ def migrate_qoqa_order_addresses(ctx):
 
     filepath = 'data/migration/order_address.csv'
     __, rows = read_csv(resource_stream(req, filepath))
+
+    count = 0
+    rows = list(rows)
+    tstart = time.time()
     for qoqa_order_id, qoqa_shipping_id, qoqa_billing_id in rows:
+        count += 1
         ctx.env.cr.execute("""
             SELECT o.id as order_id,
                    o.partner_invoice_id,
@@ -78,41 +85,44 @@ def migrate_qoqa_order_addresses(ctx):
             WHERE qo.qoqa_id = %s
         """, (qoqa_order_id,))
         row = ctx.env.cr.dictfetchone()
-        if not row:
-            continue
-        # if (row['partner_shipping_id'] != row['partner_invoice_id'] and
-        #         qoqa_shipping_id == qoqa_billing_id):
+        if row:
 
-        if not row['inv_binding_id']:
-            # not a qoqa address, keep it
-            new_invoice_id = None
-        elif row['inv_done']:
-            # already done in a previous migration
-            new_invoice_id = None
-        else:
-            new_invoice_id = copy_address(row['partner_invoice_id'],
-                                          qoqa_billing_id)
+            if not row['inv_binding_id']:
+                # not a qoqa address, keep it
+                new_invoice_id = None
+            elif row['inv_done']:
+                # already done in a previous migration
+                new_invoice_id = None
+            else:
+                new_invoice_id = copy_address(row['partner_invoice_id'],
+                                              qoqa_billing_id)
 
-        if not row['ship_binding_id']:
-            # not a qoqa address, keep it
-            new_invoice_id = None
-        elif row['ship_done']:
-            # already done in a previous migration
-            new_shipping_id = None
-        else:
-            new_shipping_id = copy_address(row['partner_shipping_id'],
-                                           qoqa_shipping_id)
+            if not row['ship_binding_id']:
+                # not a qoqa address, keep it
+                new_invoice_id = None
+            elif row['ship_done']:
+                # already done in a previous migration
+                new_shipping_id = None
+            else:
+                new_shipping_id = copy_address(row['partner_shipping_id'],
+                                               qoqa_shipping_id)
 
-        order_id = row['order_id']
-        if (new_invoice_id or new_shipping_id):
-            ctx.env.cr.execute("""
-                UPDATE sale_order
-                SET partner_invoice_id = %s,
-                    partner_shipping_id = %s
-                WHERE id = %s
-            """, (new_invoice_id or row['partner_invoice_id'],
-                  new_shipping_id or row['partner_shipping_id'],
-                  order_id))
+            order_id = row['order_id']
+            if (new_invoice_id or new_shipping_id):
+                ctx.env.cr.execute("""
+                    UPDATE sale_order
+                    SET partner_invoice_id = %s,
+                        partner_shipping_id = %s
+                    WHERE id = %s
+                """, (new_invoice_id or row['partner_invoice_id'],
+                      new_shipping_id or row['partner_shipping_id'],
+                      order_id))
+
+        if count % 1000 == 0:
+            ctx.env.cr.commit()
+            tdiff =  time.time() - tstart
+            ctx.log_line('%d rows (%.2fs)...' % (count, tdiff))
+            tstart = time.time()
 
 
 @anthem.log

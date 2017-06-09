3b609b1c0d5dcdb15fd3102fd7a9f02369158547
CharlineDumontet <charline.dumontet@camptocamp.com>
2017-06-09 12:00:50 +0200

    [BIZ-117] Better to override force_transfer

--- odoo/local-src/specific_fct/models/stock_batch_picking.py
+++ odoo/local-src/specific_fct/models/stock_batch_picking.py
@@ -3,7 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import fields, models
-from openerp import api
 
 
 class StockBatchPicking(models.Model):
@@ -16,16 +15,3 @@ class StockBatchPicking(models.Model):
         help="The active field allows you to hide the picking dispatch "
              "without removing it."
     )
-
-    @api.multi
-    def action_transfer(self):
-        """
-            Force the qty_done to always be equal to product_qty with
-            force_qty=True in force_transfert
-        """
-        batches = self.get_not_empties()
-        for batch in batches:
-            if not batch.verify_state():
-                batch.active_picking_ids.force_transfer(
-                    force_qty=True
-                )

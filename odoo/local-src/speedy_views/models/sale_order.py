73261bbda0bec98a5f5a45b70dbb34d01be3e90b
mdietrichc2c <matthieu.dietrich@camptocamp.com>
2017-06-13 14:37:01 +0200

    Improve performance for invoice validation and batch picking display

--- odoo/local-src/speedy_views/models/sale_order.py
+++ odoo/local-src/speedy_views/models/sale_order.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
-from openerp import fields, models
+from openerp import fields, models, api
 from .utils import create_index
 
 
@@ -14,8 +14,72 @@ class SaleOrder(models.Model):
     # (we remove main_exception_id from the sort)
     _order = 'date_order desc, id desc'
 
+    # Separate 2 computes from the main module ; invoice_ids will not be
+    # called 4 times during an invoice validation, and storing the invoices
+    # link / count on the SO improves the speed on the form view
+    @api.depends('name',
+                 'order_line.invoice_lines.invoice_id.type',
+                 'order_line.invoice_lines.invoice_id.origin',
+                 'order_line.invoice_lines.invoice_id.number')
+    def _get_invoice_ids(self):
+        for order in self:
+            invoice_ids = order.order_line.mapped(
+                'invoice_lines'
+            ).mapped(
+                'invoice_id'
+            ).filtered(lambda r: r.type in ['out_invoice', 'out_refund'])
+            # Search for invoices which have been 'cancelled'
+            # (filter_refund = 'modify' in 'account.invoice.refund')
+            # use like as origin may contains multiple references
+            # (e.g. 'SO01, SO02')
+            refunds = invoice_ids.search([('origin', 'like', order.name)])
+            invoice_ids |= refunds.filtered(
+                lambda r: order.name in [origin.strip() for origin
+                                         in r.origin.split(',')]
+            )
+            # Search for refunds as well
+            refund_ids = self.env['account.invoice'].browse()
+            if invoice_ids:
+                for inv in invoice_ids:
+                    refund_ids += refund_ids.search(
+                        [('type', '=', 'out_refund'),
+                         ('origin', '=', inv.number), ('origin', '!=', False),
+                         ('journal_id', '=', inv.journal_id.id)]
+                    )
+
+            order.update({
+                'invoice_count': len(set(invoice_ids.ids + refund_ids.ids)),
+                'invoice_ids': invoice_ids.ids + refund_ids.ids
+            })
+
+    @api.depends('state', 'order_line.invoice_status')
+    def _get_invoiced(self):
+        for order in self:
+            line_invoice_status = [line.invoice_status for line
+                                   in order.order_line]
+
+            if order.state not in ('sale', 'done'):
+                invoice_status = 'no'
+            elif any(invoice_status == 'to invoice'
+                     for invoice_status in line_invoice_status):
+                invoice_status = 'to invoice'
+            elif all(invoice_status == 'invoiced'
+                     for invoice_status in line_invoice_status):
+                invoice_status = 'invoiced'
+            elif all(invoice_status in ['invoiced', 'upselling']
+                     for invoice_status in line_invoice_status):
+                invoice_status = 'upselling'
+            else:
+                invoice_status = 'no'
+
+            order.update({
+                'invoice_status': invoice_status
+            })
+
+    invoice_count = fields.Integer(compute='_get_invoice_ids')
+    invoice_ids = fields.Many2many(compute='_get_invoice_ids')
     # often used in searches, 'sales to invoice' menu...
-    invoice_status = fields.Selection(index=True)
+    invoice_status = fields.Selection(compute='_get_invoiced', index=True)
 
     def init(self, cr):
 
--- odoo/local-src/speedy_views/models/stock_pack_operation.py
+++ odoo/local-src/speedy_views/models/stock_pack_operation.py
@@ -4,6 +4,7 @@
 
 
 from openerp import fields, models
+from .utils import create_index
 
 
 class StockPackOperation(models.Model):
@@ -11,3 +12,9 @@ class StockPackOperation(models.Model):
 
     picking_id = fields.Many2one(index=True)
     product_id = fields.Many2one(index=True)
+
+    def init(self, cr):
+        # index for result_package and product (from Odoo SA)
+        index_name = 'stock_pack_operation_package_product_index'
+        create_index(cr, index_name,
+                     self._table, '(result_package_id, product_id)')

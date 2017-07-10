a800dce10d90d1491bd8af9d60f524c983f3a6c3
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2017-07-10 12:15:02 +0200

    Fix script to be more effective with sql

--- scripts/correct_negative_quantz.py
+++ scripts/correct_negative_quantz.py
@@ -10,7 +10,7 @@ import odoorpc
 import psycopg2
 import psycopg2.extras
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename='quants.log',level=logging.INFO)
 
 ADMIN_USERS = ('admin_ch', 'admin_fr')
 
@@ -43,20 +43,37 @@ def get_faulty_locations_and_products(conn, company_id):
     """Get all products and stock locations pair that have
     negative quants and an available qty that is 0.
     The pair are filtered by company"""
-    sql = """SELECT sum(qty) AS cmp_qty, product_id, stock_quant.location_id
+    locations_and_products = defaultdict(list)
+    sql = """Select distinct stock_quant.location_id, product_id FROM stock_quant
+    JOIN stock_location
+        ON (stock_quant.location_id = stock_location.id)
+    WHERE stock_location.company_id = %s
+       AND stock_location.active = true
+       AND product_id in (
+
+SELECT product_id
     FROM stock_quant JOIN stock_location
     ON (stock_quant.location_id = stock_location.id)
       WHERE product_id = ANY (select distinct product_id from stock_quant where qty < 0)
       AND stock_location.company_id = %s
       AND stock_location.active = true
-      GROUP BY product_id, stock_quant.location_id
-        HAVING sum(qty) = 0"""
-    locations_and_products = defaultdict(list)
+      GROUP BY product_id
+        HAVING sum(qty) = 0)"""
+
     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cr:
-        cr.execute(sql, (company_id,))
+        cr.execute(sql, (company_id, company_id))
         for row in cr.fetchall():
             locations_and_products[row['location_id']].append(row['product_id'])
     return locations_and_products
+
+
+def clean_inventory_lines(conn, inventory_id, product_ids):
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cr:
+        logging.info('Deleting correct inventory lines')
+        sql = """Delete FROM stock_inventory_line WHERE inventory_id = %s
+                    AND product_id not in %s"""
+        cr.execute(sql, (inventory_id, tuple(product_ids)))
+        conn.commit()
 
 
 def fix_stock(config):
@@ -77,9 +94,10 @@ def fix_stock(config):
         Location = cli.env['stock.location']
         for location_id, product_ids in stock_to_fix.iteritems():
             location = Location.browse(location_id)
-            logging.info("Creating inventory for location {}".format(location.display_name))
+            logging.info(u"Creating inventory for location {}".format(
+                location.name.encode('ascii', 'replace')))
             inv_id = Inventory.create({
-                'name': "Corr. quants negatifs pour {}".format(location.name),
+                'name': u"Corr. quants negatifs pour {}".format(location.name),
                 'location_id': location_id,
                 'filter': 'none',
                 'company_id': company_id,
@@ -87,9 +105,8 @@ def fix_stock(config):
             inv = Inventory.browse(inv_id)
             inv.prepare_inventory()
             inv = Inventory.browse(inv_id)
-            for inv_line in inv.line_ids:
-                if inv_line.product_id.id not in product_ids:
-                    inv_line.unlink()
+            clean_inventory_lines(conn, inv_id, product_ids)
+            inv = Inventory.browse(inv_id)
             inv.reset_real_qty()
             inv.action_done()
 

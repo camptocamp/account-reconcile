8960de1b7cfb50ac16af5ebc4ab19e7f03a885c0
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2017-08-16 12:00:09 +0200

    Fix vat correction script [ci skip]
    to mannage corner cases

--- scripts/fix_qoqa_account_offer_invoice-v10.py
+++ scripts/fix_qoqa_account_offer_invoice-v10.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+import logging
 import argparse
 import ConfigParser
 import odoorpc
-##
+
+
+FORMAT = '%(asctime)s --  %(message)s'
+logging.basicConfig(filename='manual_invoice.log',
+                    level=logging.INFO, format=FORMAT)
 
 
 def get_config(path, env):
@@ -22,13 +27,38 @@ def rpc_client(config):
                           protocol=config['protocol'],
                           port=config['port'],
                           timeout=None)
-    client.login(config['database'],
-                 config['erp_user'],
-                 config['erp_pwd'])
+    client.login(config['db'],
+                 'admin_ch',
+                 config['admin_ch'])
     return client
 
 
+def unreconcile_from_invoice(invoice, accumulator):
+    try:
+        for move in invoice.payment_move_line_ids:
+            move.remove_move_reconcile()
+            if (move.account_id.type in ('receivable', 'payable') and
+                    move.account_id.reconcile):
+                accumulator.append(move.account_id)
+        invoice.action_cancel()
+        invoice.action_cancel_draft()
+    except:
+        logging.info("Invoice {} must be processed manually".format(invoice.number))
+
+
+def run_mass_reconcile(odoo, accounts):
+
+    AccountMassReconcile = odoo.env['account.mass.reconcile']
+    for account in accounts:
+        mass_reconcile = AccountMassReconcile.browse(
+            AccountMassReconcile.search([('account', '=', account.id)])
+        )
+        mass_reconcile.run_reconcile()
+
+
 def fix_invoice_offer(offer_list=[], config=False):
+    logging.info('---------START------------')
+    account_accumulator = []
     odoo = rpc_client(config)
     # We reconpute all refund
     Invoice = odoo.env['account.invoice']
@@ -39,7 +69,7 @@ def fix_invoice_offer(offer_list=[], config=False):
     invoice_ids = Invoice.search([('offer_id', 'in', offer_ids),
                                   ('company_id', '=', 3),
                                   ('type', '=', 'out_invoice'),
-                                  ('state', 'in', ['open','paid'])])
+                                  ('state', 'in', ['open', 'paid'])])
     cpt = 1
     len_invoices = len(invoice_ids)
     #  account_id
@@ -78,13 +108,20 @@ def fix_invoice_offer(offer_list=[], config=False):
                 if len(list(set(all_account))) > 1:
                     # If we have different account on move
                     # it's an error so skip this reconcile
-                    print "SKIP INVOICE"
+
+                    logging.info("Invoice {} must be processed manually".format(invoice.number))
                     skip_invoice = True
+                    continue
                 else:
                     move_line.remove_move_reconcile()
         # trick the invoice as computed field is not updated on time
-        invoice.action_cancel()
-        invoice.action_cancel_draft()
+        try:
+            invoice.action_cancel()
+            invoice.action_cancel_draft()
+        except Exception:
+            unreconcile_from_invoice(invoice, account_accumulator)
+            continue
+
         for invoice_line in invoice_lines:
             if invoice_line.product_id.taxes_id !=\
                     invoice_line.invoice_line_tax_ids:
@@ -113,6 +150,7 @@ def fix_invoice_offer(offer_list=[], config=False):
             move_lines = MoveLine.browse(to_reconcile_ids)
             move_lines.reconcile()
         cpt += 1
+    run_mass_reconcile(odoo, account_accumulator)
 
 
 if __name__ == '__main__':

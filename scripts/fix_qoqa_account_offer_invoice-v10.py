dd48f3a994389c478a62bb1ea197f01b8380a040
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2017-08-22 13:28:49 +0200

    [ci skip] add worker in vat script

--- scripts/fix_qoqa_account_offer_invoice-v10.py
+++ scripts/fix_qoqa_account_offer_invoice-v10.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import time
+from Queue import Queue, Empty
+from threading import Thread, Lock
 import os
 import cPickle
 import logging
@@ -13,10 +16,13 @@ logging.basicConfig(filename='manual_invoice.log',
 
 SAVESTATE_PATH = 'processed_invoice.pickle'
 
+WORKERS = 3
+
 
 class SaveStateManger(object):
 
     def __init__(self):
+        self.file_lock = Lock()
         if os.path.exists(SAVESTATE_PATH):
             with open(SAVESTATE_PATH, 'rb') as save_state_file:
                 processed_invoices = cPickle.load(save_state_file)
@@ -33,10 +39,14 @@ class SaveStateManger(object):
 
     def add(self, invoice_id):
         self.processed_invoices.append(invoice_id)
+        if not len(self.processed_invoices) % 50:
+            self.save()
 
     def save(self):
+        self.file_lock.acquire()
         with open(SAVESTATE_PATH, 'wb') as save_state_file:
             cPickle.dump(self.processed_invoices, save_state_file)
+        self.file_lock.release()
 
 
 def get_config(path, env):
@@ -62,43 +72,132 @@ def rpc_client(config):
     return client
 
 
-def unreconcile_from_invoice(invoice, save_manager):
-    try:
-        for move in invoice.payment_move_line_ids:
-            move.remove_move_reconcile()
-        invoice.action_cancel()
-        invoice.action_cancel_draft()
-    except:
-        logging.info("Can not unreconcile - {} must be processed manually".format(invoice.number))
-    finally:
-        save_manager.add(invoice.id)
+def unreconcile_from_invoice(odoo, invoice, save_manager, work_queue):
+    inv_id = invoice.id
+    if invoice.move_id:
+        print "try to unreconcile invoice {}".format(invoice.number)
+        try:
+            move = invoice.move_id
+            for move_line in move.line_ids:
+                move_line.remove_move_reconcile()
+            invoice.action_cancel()
+            invoice.action_cancel_draft()
+
+        except odoorpc.error.RPCError as exc:
+            if exc.info.get('data', {}).get('name') == 'psycopg2.OperationalError':
+                raise
+            try:
+                Invoice = odoo.env['account.invoice']
+                # we force rebowse
+                invoice = Invoice.browse(inv_id)
+                for move in invoice.payment_move_line_ids:
+                    move.remove_move_reconcile()
+                invoice.action_cancel()
+                invoice.action_cancel_draft()
+            except odoorpc.error.RPCError as exc:
+                raise
 
 
-def run_mass_reconcile(odoo, accounts):
+def move_lines_are_valid(odoo, invoice, save_manager):
+    MoveLine = odoo.env['account.move.line']
+    full_reconcile = []
+    partial_reconcile = []
+    partial_reconcile2 = []
+    if invoice.move_id:
+        move = invoice.move_id
+        for move_line in move.line_ids:
+            if move_line.full_reconcile_id:
+                full_reconcile += MoveLine.search(
+                    [('full_reconcile_id', '=',
+                      move_line.full_reconcile_id.id),
+                     ('id', '!=', move_line.id)])
+                # Check if full reconcilation is on the same
+            partial_reconcile += move_line.matched_credit_ids.\
+                credit_move_id.ids
+            partial_reconcile2 += move_line.matched_debit_ids.\
+                debit_move_id.ids
+            all_account = [x.account_id.id for
+                           x in MoveLine.browse(full_reconcile +
+                                                partial_reconcile +
+                                                partial_reconcile2)]
+            if len(list(set(all_account))) > 1:
+                # If we have different account on move
+                # it's an error so skip this reconcile
 
-    AccountMassReconcile = odoo.env['account.mass.reconcile']
-    for account in accounts:
-        mass_reconcile = AccountMassReconcile.browse(
-            AccountMassReconcile.search([('account', '=', account.id)])
-        )
-        mass_reconcile.run_reconcile()
+                logging.info(
+                    "Different accounts - Invoice {} must be processed manually".format(invoice.number)
+                )
+                save_manager.add(invoice.id)
+                return False
+    return True
+
+
+def unreconcile_worker(save_manager, config, work_queue):
+    odoo = rpc_client(config)
+    Invoice = odoo.env['account.invoice']
+    InvoiceLine = odoo.env['account.invoice.line']
+    while "Ureconciling invoices":
+        try:
+            inv_id = work_queue.get_nowait()
+        except Empty:
+            print "worker stopped"
+            return
+        if inv_id in save_manager:
+            print 'SKIPPED id {}'.format(inv_id)
+            work_queue.task_done()
+            continue
+        invoice = Invoice.browse(inv_id)
+        print "working on: invoice number %s" % (invoice.name)
+        if not move_lines_are_valid(odoo, invoice, save_manager):
+            work_queue.task_done()
+            continue
+        # trick the invoice as computed field is not updated on time
+        try:
+            unreconcile_from_invoice(odoo, invoice, save_manager, work_queue)
+            invoice = Invoice.browse(inv_id)
+            invoice_line_ids = InvoiceLine.search(
+            [('invoice_id', '=', inv_id)])
+            invoice_lines = InvoiceLine.browse(invoice_line_ids)
+            for invoice_line in invoice_lines:
+                if invoice_line.product_id.taxes_id !=\
+                        invoice_line.invoice_line_tax_ids:
+                    print "REWRITE TAXES for invoice {}".format(
+                        invoice.name)
+                    tax_tab = [x.id for x in invoice_line.product_id.taxes_id]
+                    invoice_line.sale_line_ids.write(
+                        {'tax_id': [(6, 0, tax_tab)]})
+                    invoice_line.write(
+                        {'invoice_line_tax_ids': [(6, 0, tax_tab)]}
+                    )
+                    print "REWRITE TAXES for invoice {} done".format(
+                        invoice.name)
+            invoice.signal_workflow('invoice_open')
+            save_manager.add(invoice.id)
+            work_queue.task_done()
+        except odoorpc.error.RPCError as exc:
+            if exc.info.get('data', {}).get('name') == 'psycopg2.OperationalError':
+                print "Cursor error for invoice {} back in queue".format(
+                    invoice.name)
+                work_queue.put(inv_id)
+                work_queue.task_done()
+                continue
+            else:
+                logging.info("Unexpected error - {} must be processed manually".format(invoice.name))
+                save_manager.add(invoice.id)
 
 
 def fix_invoice_offer(offer_list, save_manager, config=False):
+    work_queue = Queue()
     logging.debug('---------START------------')
     odoo = rpc_client(config)
     # We reconpute all refund
     Invoice = odoo.env['account.invoice']
     QoqaOffer = odoo.env['qoqa.offer']
-    InvoiceLine = odoo.env['account.invoice.line']
-    MoveLine = odoo.env['account.move.line']
     offer_ids = QoqaOffer.search([('ref', 'in', offer_list)])
     invoice_ids = Invoice.search([('offer_id', 'in', offer_ids),
                                   ('company_id', '=', 3),
                                   ('type', '=', 'out_invoice'),
                                   ('state', 'in', ['open', 'paid'])])
-    cpt = 1
-    len_invoices = len(invoice_ids)
     #  account_id
     #   id  | code
     # ------+-------
@@ -106,68 +205,31 @@ def fix_invoice_offer(offer_list, save_manager, config=False):
     #  2702 | 32001
     #  2706 | 32005
     for inv_id in invoice_ids:
-        if inv_id in save_manager:
-            print 'SKIPPED id {}'.format(inv_id)
-            cpt += 1
-            continue
-        invoice = Invoice.browse(inv_id)
-        print str("%s on %s: number %s" % (cpt, len_invoices, invoice.name))
-        invoice_line_ids = InvoiceLine.search(
-            [('invoice_id', '=', inv_id)])
-        invoice_lines = InvoiceLine.browse(invoice_line_ids)
-        full_reconcile = []
-        partial_reconcile = []
-        partial_reconcile2 = []
-        if invoice.move_id:
-            move = invoice.move_id
-            for move_line in move.line_ids:
-                if move_line.full_reconcile_id:
-                    full_reconcile += MoveLine.search(
-                        [('full_reconcile_id', '=',
-                          move_line.full_reconcile_id.id),
-                         ('id', '!=', move_line.id)])
-                    # Check if full reconcilation is on the same
-                partial_reconcile += move_line.matched_credit_ids.\
-                    credit_move_id.ids
-                partial_reconcile2 += move_line.matched_debit_ids.\
-                    debit_move_id.ids
-                all_account = [x.account_id.id for
-                               x in MoveLine.browse(full_reconcile +
-                                                    partial_reconcile +
-                                                    partial_reconcile2)]
-                if len(list(set(all_account))) > 1:
-                    # If we have different account on move
-                    # it's an error so skip this reconcile
-
-                    logging.info(
-                        "Different accounts - Invoice {} must be processed manually".format(invoice.number)
-                    )
-                    save_manager.add(invoice.id)
-                    continue
-                else:
-                    move_line.remove_move_reconcile()
-        # trick the invoice as computed field is not updated on time
-        try:
-            invoice.action_cancel()
-            invoice.action_cancel_draft()
-        except Exception:
-            unreconcile_from_invoice(invoice, save_manager)
-            continue
-
-        for invoice_line in invoice_lines:
-            if invoice_line.product_id.taxes_id !=\
-                    invoice_line.invoice_line_tax_ids:
-                print str("REWRITE TAXES")
-                tax_tab = [x.id for x in invoice_line.product_id.taxes_id]
-                invoice_line.sale_line_ids.write(
-                    {'tax_id': [(6, 0, tax_tab)]})
-                invoice_line.write({'invoice_line_tax_ids': [(6, 0, tax_tab)]})
-
-        invoice.signal_workflow('invoice_open')
-        save_manager.add(invoice.id)
-        cpt += 1
-        if not cpt % 1:
-            save_manager.save()
+        work_queue.put(inv_id)
+    workers = []
+    for _ in range(WORKERS):
+        worker = Thread(target=unreconcile_worker,
+                        args=(save_manager, config, work_queue))
+        workers.append(worker)
+        worker.setDaemon(True)
+        worker.start()
+    try:
+        while True:
+            if not work_queue.empty():
+                time.sleep(1)
+            else:
+                break
+    except KeyboardInterrupt:
+        print "stopping threads"
+        while "stopping threads":
+            try:
+                work_queue.get_nowait()
+                work_queue.task_done()
+            except Empty:
+                break
+        for w in workers:
+            w.join()
+        exit()
 
 
 if __name__ == '__main__':

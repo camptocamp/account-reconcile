d9c6017a9b8da38e6d0d2f67ba8aad5afe1ef8d9
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-11-06 17:58:34 +0100

    Move migration step in next version and add update of modules

--- odoo/songs/upgrade/9_36_0.py
+++ odoo/songs/upgrade/9_36_0.py
@@ -6,16 +6,6 @@ from anthem.lyrics.uninstaller import uninstall
 
 
 @anthem.log
-def update_crm_stage_sequences(ctx):
-    """ Update stage sequences to add new stage in between """
-    # those sequences have tag no update that's why we need
-    # to update them manually
-    ctx.env.ref('crm_claim.stage_claim2').sequence = 29
-    ctx.env.ref('crm_claim.stage_claim3').sequence = 30
-
-
-@anthem.log
 def main(ctx):
     # we will uninstalled modules
     uninstall(ctx, ['password_security'])
-    update_crm_stage_sequences(ctx)

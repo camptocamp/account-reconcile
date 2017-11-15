d833903ab7a0de05b8b0bd997c48ac4885629ecf
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2017-11-15 07:37:46 +0100

    PEP8

--- odoo/songs/upgrade/9_37_0.py
+++ odoo/songs/upgrade/9_37_0.py
@@ -1,7 +1,10 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-
 import anthem
+<<<<<<< c775c3cab0d6b152f691ea6d31c5b10bebf97abf
+=======
+
+>>>>>>> PEP8
 
 
 @anthem.log

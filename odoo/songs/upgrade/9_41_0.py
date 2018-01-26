84215164698952124a21e94a3ce2df6b86296ff7
oleksandrpaziuk <o.paziuk@mobilunity.com>
2018-01-26 19:13:35 +0200

    [FIX][BIZ-403] add correct filters to claim

--- odoo/songs/upgrade/9_41_0.py
+++ odoo/songs/upgrade/9_41_0.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 import anthem
@@ -15,6 +16,25 @@ def enable_issuance_import_cron(ctx):
     WHERE module = 'connector_qoqa'
     AND name = 'ir_cron_qoqa_import_discount_accounting')
     """)
+
+
+@anthem.log
+def fix_sva_filters(ctx):
+    """ Fix SVA filters """
+    todo_filter = ctx.env.ref(
+        'crm_claim_mail.todo_filter', raise_if_not_found=False
+    )
+    if todo_filter:
+        todo_filter.unlink()
+
+    ctx.env['ir.filters'].search(
+        [('name', 'ilike', 'Non traité depuis 48h')]
+    ).unlink()
+
+
+@anthem.log
+def pre(ctx):
+    fix_sva_filters(ctx)
 
 
 @anthem.log

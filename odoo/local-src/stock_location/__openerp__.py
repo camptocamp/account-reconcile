5f62f21c792e7e7c85f5ea41e0b2bf6c258fb7ab
Denis Leemann <denis.leemann@camptocamp.com>
2018-04-12 18:56:46 +0200

    bsqoq-49 add filters for empty locations

---
+++ odoo/local-src/stock_location/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{'name': 'Stock Location',
+ 'version': '9.0.1.0.0',
+ 'author': 'Camptocamp',
+ 'license': 'AGPL-3',
+ 'category': 'stock',
+ 'depends': [
+     'stock',
+ ],
+ 'website': 'http://www.camptocamp.com',
+ 'data': [
+     'views/stock.xml',
+ ],
+ 'installable': True,
+ }

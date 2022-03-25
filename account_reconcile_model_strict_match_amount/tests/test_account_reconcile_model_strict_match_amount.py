d13a15adc5c82b0937e9b2b19c9cd3a97e80e759
Akim Juillerat <akim.juillerat@camptocamp.com>
2022-03-25 13:21:40 +0100

    Fix account_reconcile_model_strict_match_amount tests
    Tests were broken after introduction of new commit on Odoo.
    Since account.move.line.ref field was set as readonly, writing
    a value on it did not set the value on account.move.ref (which
    it's related to) anymore.
    cf https://github.com/odoo/odoo/commit/38c13bbef39e3e82a77cfd4a316be94280e3cee5

--- account_reconcile_model_strict_match_amount/tests/test_account_reconcile_model_strict_match_amount.py
+++ account_reconcile_model_strict_match_amount/tests/test_account_reconcile_model_strict_match_amount.py
@@ -16,7 +16,10 @@ class TestAccountReconcileModelStrictMatchAmount(TestReconciliationMatchingRules
         cls.invoice_line_s_5 = cls._create_invoice_line(
             150, cls.partner_s_3, "out_invoice"
         )
-        cls.invoice_line_s_5.ref = "ABC001XYZ"
+        # Explicitly set the ref on the account.move after field on the line
+        # was defined as readonly and value is not written anymore
+        # cf https://github.com/odoo/odoo/commit/38c13bbef39e3e82a77cfd4a316be94280e3cee5  # noqa
+        cls.invoice_line_s_5.move_id.ref = "ABC001XYZ"
         cls.invoice_line_s_6 = cls._create_invoice_line(
             300, cls.partner_s_4, "out_invoice"
         )
